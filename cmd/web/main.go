42b4f4acae7a418d6fc9340bcec818ca48070604
cherigra <cherigra@student.21-school.ru>
2024-04-06 22:29:42 +0300

    main

---
+++ cmd/web/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+	// "github.com/test/pkg/models/postgre"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	// commands *postgre.CommandModel
+}
+
+func main() {
+	addr := flag.String("addr", ":8080", "Сетевой адрес веб-сервера")
+	// dsn := flag.String("dsn", "host=localhost port=5432 user=postgres password=1 dbname=postgres sslmode=disable", "Название PostgreSQL источника данных")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// db, err := openDB(*dsn)
+	// if err != nil {
+	// 	errorLog.Fatal(err)
+	// }
+
+	// defer db.Close()
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		// commands: &postgre.CommandModel{DB: db},
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Запуск сервера на 127.0.0.1%s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
